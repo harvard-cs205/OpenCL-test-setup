44fcf01d72bad63aeca991c3bb7c10b1f104058e
Thouis (Ray) Jones <thouis@gmail.com>
2015-10-29 13:05:59 -0400

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,10 @@
 #
 #SBATCH -p holyseasgpu
 #SBATCH --gres=gpu
-
+#SBATCH -n 1 # Number of cores
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH --gres=gpu
+#SBATCH --mem=500
 
 # add additional commands needed for Lmod and module loads here
 
@@ -13,6 +16,7 @@ module load pyopencl
 
 # add commands for analyses here
 
+python test.py
 
 # end of program
 exit 0;
